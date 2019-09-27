8a0973697ee97500c17b5e094036cd757ad098dd
LPicker <lianjianjie@foxmail.com>
2019-09-28 03:38:37 +0800

    音乐馆搜索框和轮播图

--- qqmusicvue/src/main.js
+++ qqmusicvue/src/main.js
@@ -9,6 +9,12 @@ import MintUI from "mint-ui"
 import "mint-ui/lib/style.css"
 // 3:将mint-ui注册vue实例
 Vue.use(MintUI)
+// 1:引入vant组件Swipe
+import { Swipe, SwipeItem } from 'vant';
+// 2:单引入vant库中样式文件
+import "vant/lib/index.css"
+// 3:将Swipe/SwipeItem注册vue实例
+Vue.use(Swipe).use(SwipeItem);
 
 Vue.config.productionTip = false
 
---
+++ qqmusicvue/src/views/musicHall.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <searchbar></searchbar>
+        <swipe></swipe>
+    </div>
+</template>
+<script>
+import SearchBar from "../components/common/SearchBar"
+import Swipe from "../components/common/Swipe"
+export default {
+    components:{
+        "searchbar":SearchBar,
+        "swipe":Swipe
+    }
+}
+</script>
+<style>
+    body{
+        background-color:#f3f3f3;
+    }
+</style>
