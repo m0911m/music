b5a533019e1b973b73606388c02ad664fd4a3e6b
m0911m <m0911k@163.com>
2019-09-27 10:14:13 +0800

    引入mint-ui组件

--- qqmusicvue/src/main.js
+++ qqmusicvue/src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 功能:引入第三方组件库mint-ui
+// 1:完整引入mint-ui组件库中所有组件
+import MintUI from "mint-ui"
+// 2:单引入mint-ui库中样式文件
+import "mint-ui/lib/style.css"
+// 3:将mint-ui注册vue实例
+Vue.use(MintUI)
 
 Vue.config.productionTip = false
 
