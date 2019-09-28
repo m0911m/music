1c4c62684cac972f615663f98ea6317de7169004
DongQiannan <18291867058@163.com>
2019-09-28 17:45:22 +0800

    小卡片

---
+++ qqmusicvue/src/components/common/DynamicCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <!-- 1.上面的小图片和用户名，关注 -->
+    <!-- 1.1左上角的图标和用户名 -->
+    <div class="top">
+      <img :src="upic" alt="">
+      <span>{{uname}}</span>
+      <input type="text" :placeholder='focused?notyet:already' v-model="active">
+    </div>
+    <!-- 1.2右上角的+关注 -->
+    <!-- 2.中间的大图 -->
+    <!-- 3、下面的备注 -->
+  </div>
+</template>
+<script>
+export default {
+  upic:{default:""},
+  uname:{default:""},
+  focused:false,
+  notyet:{default:"+关注"},
+  already:{default:"已关注"}
+}
+</script>
+<style scoped>
+.container{
+  display:flex;
+}
+.top{
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+  
+}
+</style>
+
--- qqmusicvue/src/views/Dynamic.vue
+++ qqmusicvue/src/views/Dynamic.vue
@@ -2,15 +2,18 @@
   <div>
     <dynamicbar :dynamicIcon="require('../assets/focus.png')"></dynamicbar>
     <swipe></swipe>
+    <dynamiccard uname="比三呆" :upic="require('../assets/uboy.png')"></dynamiccard>
   </div>
 </template>
 <script>
 import DynamicBar from '../components/common/DynamicBar.vue'
 import Swipe from '../components/common/Swipe.vue'
+import DynamicCard from '../components/common/DynamicCard.vue'
 export default {
   components:{
     "dynamicbar":DynamicBar,
-    "swipe":Swipe
+    "swipe":Swipe,
+    "dynamiccard":DynamicCard
   }
 }
 </script>
