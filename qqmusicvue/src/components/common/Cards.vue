d1377b4fcb8afa6b5f115ae148f3c63be876669a
LPicker <lianjianjie@foxmail.com>
2019-09-30 00:15:08 +0800

    jz card

--- qqmusicvue/src/components/common/Cards.vue
+++ qqmusicvue/src/components/common/Cards.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="cards">
-        <div class="card" v-for="(pic,i) of pics" :key="i">
-            <img :src="require('../../assets/CsYa.jpg')" class="imgstyle">
+        <div class="card" :style="`background-color:${card.cardcolor}`" v-for="(card,i) of cards" :key="i">
+            <img :src="require(`../../assets/${card.pic}`)" class="imgstyle">
             <div class="cardtext">
-                <span>{{title}}</span>
-                <span>{{subtitle}}</span>
+                <span class="title">{{card.title}}</span>
+                <span class="subtitle">{{card.subtitle}}</span>
             </div>
         </div>
     </div>
@@ -14,9 +14,20 @@
 export default {
     data(){
         return{
-            pics:["CsYa.jpg","AfYu.jpg"],
-            title:["新歌新碟","数字专辑"],
-            subtitle:["麻辣鸡退休?NO!","限定记忆序章"]
+            cards:[
+                {
+                    pic:"CsYa.jpg",
+                    title:"新歌新碟",
+                    subtitle:"麻辣鸡退休?NO!",
+                    cardcolor:"#3bc79a"
+                },
+                 {
+                    pic:"AfYu.jpg",
+                    title:"数字专辑",
+                    subtitle:"限定记忆序章",
+                    cardcolor:"#fbc53f"
+                }
+            ]
         }
     }
 }
@@ -31,17 +42,26 @@ export default {
 .card{
     display:flex;
     width:10.7rem;
-    height:5rem;
-    background-color:#31c27c;
-    border-radius:5%;
+    height:4.7rem;
+    border-radius:0.3rem;
 }
 .cardtext{
     display:inline-flex;
     flex-direction:column;
     align-items:flex-start;
+    color:#fff;
 }
 .imgstyle{
-    width:3.5rem;
-    height:3.5rem;
+    width:3rem;
+    height:3rem;
+    margin:0.7rem 0 0 0.7rem;
+}
+.title{
+    font-weight: bold;
+    margin:0.9rem 0 0.5rem 0.4rem;
+}
+.subtitle{
+    font-size: 0.85rem;
+    margin-left: 0.4rem;
 }
 </style>
