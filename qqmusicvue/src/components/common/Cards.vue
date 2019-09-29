467d0f6c523110a646f3c8dd637530a53378dded
LPicker <lianjianjie@foxmail.com>
2019-09-30 00:50:30 +0800

    jz card rev

--- qqmusicvue/src/components/common/Cards.vue
+++ qqmusicvue/src/components/common/Cards.vue
@@ -1,67 +1,73 @@
 <template>
-    <div class="cards">
-        <div class="card" :style="`background-color:${card.cardcolor}`" v-for="(card,i) of cards" :key="i">
-            <img :src="require(`../../assets/${card.pic}`)" class="imgstyle">
-            <div class="cardtext">
-                <span class="title">{{card.title}}</span>
-                <span class="subtitle">{{card.subtitle}}</span>
-            </div>
+  <div class="cards">
+    <div class="card" :style="`background-color:${card.cardcolor}`"
+      v-for="(card,i) of cards" :key="i">
+      <router-link to>
+        <img :src="require(`../../assets/${card.pic}`)" class="imgstyle" />
+        <div class="cardtext">
+          <span class="title">{{card.title}}</span>
+          <span class="subtitle">{{card.subtitle}}</span>
         </div>
+      </router-link>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            cards:[
-                {
-                    pic:"CsYa.jpg",
-                    title:"新歌新碟",
-                    subtitle:"麻辣鸡退休?NO!",
-                    cardcolor:"#3bc79a"
-                },
-                 {
-                    pic:"AfYu.jpg",
-                    title:"数字专辑",
-                    subtitle:"限定记忆序章",
-                    cardcolor:"#fbc53f"
-                }
-            ]
+  data() {
+    return {
+      cards: [
+        {
+          pic: "CsYa.jpg",
+          title: "新歌新碟",
+          subtitle: "麻辣鸡退休?NO!",
+          cardcolor: "#3bc79a"
+        },
+        {
+          pic: "AfYu.jpg",
+          title: "数字专辑",
+          subtitle: "限定记忆序章",
+          cardcolor: "#fbc53f"
         }
-    }
-}
+      ]
+    };
+  }
+};
 </script>
 
 <style>
-.cards{
-    display:flex;
-    justify-content:space-around;
-    padding:1rem 0.5rem;
+.cards {
+  display: flex;
+  justify-content: space-around;
+  padding: 1rem 0.5rem;
+}
+.card a{
+  display: flex;
+  width: 10.7rem;
+  height: 4.7rem;
 }
 .card{
-    display:flex;
-    width:10.7rem;
-    height:4.7rem;
-    border-radius:0.3rem;
+    border-radius: 0.3rem;
 }
-.cardtext{
-    display:inline-flex;
-    flex-direction:column;
-    align-items:flex-start;
-    color:#fff;
+.cardtext {
+  display: inline-flex;
+  flex-direction: column;
+  align-items: flex-start;
+  color: #fff; 
 }
-.imgstyle{
-    width:3rem;
-    height:3rem;
-    margin:0.7rem 0 0 0.7rem;
+.imgstyle {
+  width: 3rem;
+  height: 3rem;
+  margin: 0.7rem 0 0 0.7rem;
 }
-.title{
-    font-weight: bold;
-    margin:0.9rem 0 0.5rem 0.4rem;
+.title {
+  font-size: 0.9rem;
+  font-weight: bold;
+  margin: 0.9rem 0 0.5rem 0.4rem;
 }
-.subtitle{
-    font-size: 0.85rem;
-    margin-left: 0.4rem;
+.subtitle {
+  font-size: 0.8rem;
+  margin-left: 0.4rem;
 }
 </style>
