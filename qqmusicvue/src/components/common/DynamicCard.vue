29cc929f0ea8e57d6beab4c2e8c7de65155dc36e
MacBook Air <365515462@qq.com>
2019-10-09 00:00:05 +0800

    动态提交

--- qqmusicvue/src/components/common/DynamicCard.vue
+++ qqmusicvue/src/components/common/DynamicCard.vue
@@ -3,49 +3,80 @@
     <!-- 1.上面的小图片和用户名，关注 -->
     <!-- 1.1左上角的图标和用户名 -->
     <div class="top">
-      <img :src="upic" alt="">
-      <span>{{uname}}</span>
+      <div class="leftText">
+        <img :src="upic" alt="">
+        <div class="uname">{{uname}}</div>
+      </div>
       <!-- 1.2右上角的+关注 -->
-      <input type="text" placeholder="+关注" v-model="active">
-    </div>
+      <input type="text" placeholder="+关注" >
+    </div> 
+    <div class="text">{{utext}}</div>
     <!-- 2.中间的大图 -->
-    <div>
-      <img :src="bigimg" alt="">
+    <div class="middle">
+      <img :src="mpic" alt="">
     </div>
     <!-- 3、下面的备注 -->
-    <div>
-      <span>###行走的CD机</span>
-
+    <div class="bottom"> 
+      <span>#ACG</span>
     </div>
   </div>
 </template>
 <script>
 export default {
-  upic:{default:""},
-  uname:{default:""},
-  bigimg:{default:""}
+  props:{
+    upic:{default:""},
+    uname:{default:""},
+    utext:{default:""},
+    mpic:{default:""}
+  }
 }
 </script>
 <style scoped>
-.container{
-  display:flex;
-}
 .top{
   display:flex;
   justify-content: space-between;
   align-items: center;
-  position: relative;
+  margin-right:0.75rem;
 }
-.top>input{
+input{
   display:flex;
   width:3.25rem;
   justify-content:space-between;
   border-radius:1rem;
   border:1px solid #ddd;
   opacity: 0.75;
-  padding:2px;
+  margin-left:12rem;
   font-size:0.8rem;
   text-align:center;
+}
+.leftText{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.leftText>img{
+  width:2rem;
+  border-radius: 1rem;
+  margin: 0.5rem 0.25rem;
+}
+.uname{
+  
+  align-items:center;
+}
+.text{
+  display: flex;
+  align-items:left;
+  margin:0.2rem 0.25rem;
+}
+.middle{
+  display:flex;
+  margin-top:0.75rem;
+  margin-left:0.5rem;
+}
+.middle img{
+  display: flex;
+  width:22rem;
+  border-radius:0.3rem;
 }
 </style>
 
--- qqmusicvue/src/views/Dynamic.vue
+++ qqmusicvue/src/views/Dynamic.vue
@@ -2,8 +2,11 @@
   <div>
     <dynamicbar :dynamicIcon="require('../assets/focus.png')"></dynamicbar>
     <swipe></swipe>
-    <dynamiccard uname="比三呆" :upic="require('../assets/uboy.png')"
-      :bigimg="require('../assets/1747238.jpg')"
+    <dynamiccard 
+      uname="月月酱"
+      :upic="require('../assets/ugirl.png')"
+      utext="╔into You╝ 想给你所有的美好"
+      :mpic="require('../assets/1748028.jpg')"
     ></dynamiccard>
   </div>
 </template>
