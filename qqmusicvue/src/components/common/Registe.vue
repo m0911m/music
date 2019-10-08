818f85d310b280b299b33e22430d752f563071b0
m0911m <m0911k@163.com>
2019-10-08 20:21:38 +0800

    嗯嗯

--- qqmusicvue/src/components/common/Registe.vue
+++ qqmusicvue/src/components/common/Registe.vue
@@ -2,11 +2,10 @@
   <div>
     <h1 style="text-align:center">注册页面</h1>
     <mt-field label="用户名" placeholder="请输入用户名" v-model="username"></mt-field>
-<mt-field label="密码" placeholder="请输入密码" type="password" v-model="password"></mt-field>
-<mt-field label="邮箱" placeholder="请输入邮箱" type="email" v-model="email"></mt-field>
-<mt-field label="手机号" placeholder="请输入手机号" type="tel" v-model="phone"></mt-field>
-
-<mt-button size="large" type="primary" @click="reg">注册</mt-button>
+    <mt-field label="密码" placeholder="请输入密码" type="password" v-model="password"></mt-field>
+    <mt-field label="邮箱" placeholder="请输入邮箱" type="email" v-model="email"></mt-field>
+    <mt-field label="手机号" placeholder="请输入手机号" type="tel" v-model="phone"></mt-field>
+    <mt-button size="large" type="primary" @click="reg">注册</mt-button>
   </div>
 </template>
 <script>
@@ -21,7 +20,15 @@ export default {
   },
   methods:{
     reg(){
-      
+    // 获取用户输入的用户名
+    var u=this.username;
+    // 获取用户输入的密码
+    var p=this.password;
+    // 获取用户输入的邮箱
+    var e=this.email
+    // 获取用户输入的手机号
+    var h=this.phone
+
     }
   }
 }
