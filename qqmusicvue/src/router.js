c2a4e42c1d2506409e5048e3da2f24b1d0f82544
music <m0911k163.com>
2019-10-08 20:50:50 +0800

    XIAO

--- qqmusicvue/src/router.js
+++ qqmusicvue/src/router.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import musicHall from './views/musicHall.vue'
 import Dynamic from './views/Dynamic.vue'
-import Wode from './components/wode.vue'
+import Wode from './components/Wode.vue'
 import Login from './components/common/Login.vue'
 import Registe from './components/common/Registe.vue'
 Vue.use(Router)
